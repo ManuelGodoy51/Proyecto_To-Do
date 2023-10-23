a7b98a858d4bef12e9b4e13a5b188ecaa19fb4a8
ManuelGodoy51 <66335741+ManuelGodoy51@users.noreply.github.com>
2023-10-23 20:11:56 -0300

    explicacion en el doc del proyecto

--- app/routes/rutas_tareas.py
+++ app/routes/rutas_tareas.py
@@ -15,6 +15,24 @@ def get_db():
 
 @router.post("/guardar")
 def Crear_tarea(tarea_data : dict, db : SessionLocal = Depends(get_db)):
+   """
+    En este módulo la manera correcta de ingresar una nueva tarea es la siguiente:
+
+    {
+        "titulo": "Título de la tarea",
+        "usuario": "usuario",
+        "descripcion": "Descripción de la tarea",
+        "fecha_vencimiento": "2023-10-30",
+        "estado": "Estado de la tarea"
+    }
+
+    La tabla en la base de datos es la siguiente, con estos tamaños en los campos:
+    - usuario = String(length=20)
+    - titulo = String(length=40)
+    - descripcion = String(length=150)
+    - fecha_vencimiento = Date
+    - estado = String(length=100
+    """
    match tarea_data:
        case _ if "usuario" not in tarea_data:
            raise HTTPException(status_code = 400, detail = "El usuario es requerido")
@@ -37,6 +55,9 @@ def Crear_tarea(tarea_data : dict, db : SessionLocal = Depends(get_db)):
 
 @router.get("/lista")
 def Listar_tareas(usuario: str = Query(None), estado: str = Query(None), db : SessionLocal = Depends(get_db)):
+    """
+        En este módulo se pueden listar todas las tareas, o bien se puede filtrar por estado y/o usuario.
+    """
     if not usuario and not estado:
         tareas = db.query(Tareas).all()
     elif usuario and not estado:
@@ -56,7 +77,23 @@ def Listar_tareas(usuario: str = Query(None), estado: str = Query(None), db : Se
 
 @router.put("/editar_tarea")
 def Editar_tareas(tarea_id: int, usuario: str, data: dict, db : SessionLocal = Depends(get_db)):
+    """
+         En este módulo se puede editar toda la tarea como tambien solo el estado de dicha tarea, y para ello es necesario contar con el id y el usuario de la tarea.
+         Para realizar el editado de la tarea completa es de la siguiente forma:
 
+         {
+            "titulo": "Título de la tarea",
+            "descripcion": "Descripción de la tarea",
+            "fecha_vencimiento": "2023-10-30",
+            "estado": "Estado de la tarea"
+         }
+
+         y para editar el estadom es asi:
+
+         {
+            "estado": "Estado de la tarea"
+         }
+    """
     if usuario == "" or usuario is None:
         raise HTTPException(status_code=404, detail="El usuario es in campo requerido")
     elif tarea_id is None:
@@ -86,6 +123,9 @@ def Editar_tareas(tarea_id: int, usuario: str, data: dict, db : SessionLocal = D
 
 @router.delete("/eliminar")
 def Eliminar_tarea(tarea_id: int, usuario: str, db : SessionLocal = Depends(get_db)):
+    """
+         En este módulo se puede eliminar la tarea y para ello es necesario el id y el usuario de la tarea:
+    """
     existe_tarea = db.query(Tareas).filter(Tareas.id == tarea_id, Tareas.usuario == usuario).first()
     if existe_tarea is None:
         raise HTTPException(status_code=404, detail="No existe la tarea")
